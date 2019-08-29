7ff9f3debfba5694c70794ece37238febb76ca1c
tracyann <tracyann@tracyann-pc.wireless-1x.unc.edu>
2019-08-29 15:46:14 -0400

    Final updated

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -5,23 +5,36 @@ import java.util.Scanner;
 public class A1Novice {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
-		int customerNumber = scan.nextInt();
-		
-		for (int i=0; i<customerNumber; i++) {
-			String customerName = scan.next();
+
+
+
+		int numberOfCustomers = scan.nextInt();
+		// this for loop takes in one customer at a time
+		for (int i=0; i<numberOfCustomers; i++) {
+			String customerFirstName = scan.next();
+			String customerLastName = scan.next();
+			char c = customerFirstName.charAt(0); 
 			int numItems = scan.nextInt();
-			int numItem = scan.nextInt();
-			int cost = scan.nextInt();
-			
+			double total = 0;
+
+			// this for loop takes in the number of items each customer has
+			for (int j=0; j<numItems; j++) {
+				int eachItem = scan.nextInt();
+				String itemName = scan.next();
+				double cost = scan.nextDouble();
+				total += eachItem*cost;
+
+
+			}
+
+			System.out.println(c + ". " + customerLastName + ": " + String.format("%.2f", total));
 		}
-		
-		
+
+
+
 	}
-	
-	
 }
